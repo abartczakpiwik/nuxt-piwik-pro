44b216d65b018ed9b93f8a2af67813ca080ea947
Szymon Sysło <s.syslo@piwik.pro>
2024-06-21 14:20:09 +0200

    Improve export

--- example/composables/usePiwikPro.ts
+++ example/composables/usePiwikPro.ts
@@ -1,3 +1,1 @@
-import { usePiwikPro as _usePiwikPro } from "@piwikpro/nuxt-piwik-pro/composables";
-
-export const usePiwikPro = _usePiwikPro;
+export { usePiwikPro } from "@piwikpro/nuxt-piwik-pro/composables";
