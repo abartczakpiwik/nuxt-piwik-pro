118315c59f5019c5b7077c8afbe10aa1287f341c
Szymon Sysło <s.syslo@piwik.pro>
2024-06-21 15:50:56 +0200

    Remove unnecessary types export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from "@piwikpro/vue-piwik-pro";
 export { default as PiwikPRO } from "@piwikpro/vue-piwik-pro";
-export * from "./types";
+export { PiwikPROServicesType, PiwikPROHandler } from "./types";
 export { default } from "./module";
